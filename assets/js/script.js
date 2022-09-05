86abca581bb771dd4b6d44619030a1e674281585
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-09-04 21:20:49 -0500

    added dynamic  user input for resolution

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,44 @@
 const sketchZoneEl = document.querySelector('.sketch-zone');
+const btnEl = document.querySelector('.btn');
 
+
+const removeDiv = (choice) =>{
+  const oldGrid = document.querySelectorAll('.grid');
+
+  oldGrid.remove();
+}
 
 
 const createDivs = () => {
 
-  for (i = 1; i < 4097; i++) {
+
+  for (i = 0; i < (10 * 10); i++) {
     const div = document.createElement('div');
     div.setAttribute('id', i);
     div.classList = 'grid';
-    
 
     sketchZoneEl.appendChild(div);
 
    
       div.addEventListener('mouseover', function hover() {
         div.classList='hovered grid';
-
-       
       })
     }
+  
+
+    btnEl.addEventListener('click', function userPrompt() {
+      const choice = prompt('Please enter a number between 10 and 100 to determine the resolution of your etch a sketch');
+
+      console.log(choice);
+
+      removeDiv(choice)
+
+    })
+
+    
 
 
   }
-
-
-
-
 
 
 window.onload = createDivs;
